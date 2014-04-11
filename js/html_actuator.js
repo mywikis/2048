bb1e529fef8e796dba764f720ed5f5f86c1d3805
mywikis <contact@mywikis.com>
2014-04-10 20:17:44 -0500

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 2048) classes.push("tile-super");
+  if (tile.value > 64) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You won! Yay?" : "Game over! :(";
+  var message = won ? "You won! :)" : "Game over! :(";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
